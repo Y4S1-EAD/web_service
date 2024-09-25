98df52b6281be5ae589c08a725df35c78ebb1b51
Sandani Thilakarathna <127828782+Sandani2000@users.noreply.github.com>
2024-09-25 06:13:24 +0530

    product api changes

--- Models/Product.cs
+++ Models/Product.cs
@@ -4,35 +4,34 @@ using System.ComponentModel.DataAnnotations;
 
 namespace web_service.Models
 {
-    public class Product
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? ProductId { get; set; }  // The null-forgiving operator - Make ProductId nullable
+   public class Product
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? ProductId { get; set; }
 
-        [Required(ErrorMessage = "ProductName is required.")]
-        public string ProductName { get; set; }
+    [Required(ErrorMessage = "ProductName is required.")]
+    public string ProductName { get; set; }
 
-        // ProductDescription is optional
-        public string ProductDescription { get; set; }
+    public string ProductDescription { get; set; }
 
-        [Required(ErrorMessage = "Price is required.")]
-        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
-        public decimal Price { get; set; }
+    [Required(ErrorMessage = "Price is required.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+    public decimal Price { get; set; }
 
-        [Required(ErrorMessage = "Stock is required.")]
-        [Range(0, int.MaxValue, ErrorMessage = "Stock cannot be negative.")]
-        public int Stock { get; set; }
+    [Required(ErrorMessage = "Stock is required.")]
+    [Range(0, int.MaxValue, ErrorMessage = "Stock cannot be negative.")]
+    public int Stock { get; set; }
 
-        // Image is optional
-        public string Image { get; set; }
+    // Image stored as a Base64 string
+    public string Image { get; set; }
 
-        [Required(ErrorMessage = "CategoryId is required.")]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string CategoryId { get; set; }
+    [Required(ErrorMessage = "CategoryId is required.")]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string CategoryId { get; set; }
 
-        [Required(ErrorMessage = "VendorId is required.")]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string VendorId { get; set; }
-    }
+    [Required(ErrorMessage = "VendorId is required.")]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string VendorId { get; set; }
+}
 }
